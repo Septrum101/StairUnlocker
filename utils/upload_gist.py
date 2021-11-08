0ba0dc8e096d8206c8b35f20a08760a7639e0059
thank243 <thank243@gmail.com>
2021-11-08 21:11:58 +0800

    Update upload_gist.py
    fix minor bugs.

--- utils/upload_gist.py
+++ utils/upload_gist.py
@@ -10,11 +10,10 @@ def upload(content):
     def create():
         create_file = requests.post("https://api.github.com/gists",
                                     data=json.dumps({"files": {"stairunlocker": {"content": content}}}),
-                                    headers={"Authorization": "token " + config['token'],
+                                    headers={"Authorization": f"token {config['token']}",
                                              "Accept": "application/vnd.github.v3+json"})
         if create_file.status_code == 201:
-            logger.info("Writing to Gist success!\nRaw URL: " +
-                        json.loads(create_file.text)['files']['stairunlocker']['raw_url'])
+            logger.info("Writing to Gist success! Please visit: https://gists.github.com for details.")
         else:
             logger.error(json.loads(create_file.text)['message'])
             raise Exception(json.loads(create_file.text)['message'])
@@ -22,11 +21,6 @@ def upload(content):
         config.pop("VERSION")
         with open('stairunlock_config.json', 'w', encoding='utf-8') as _:
             json.dump(config, _, indent=2, ensure_ascii=False)
-
-    def generate_url(raw):
-        raw = raw.split('/')
-        raw.pop(-2)
-        return "/".join(raw)
 
     if 'gistURL' not in config or config['gistURL'] == "":
         create()
@@ -36,10 +30,10 @@ def upload(content):
                                  headers={"Authorization": "token " + config['token'],
                                           "Accept": "application/vnd.github.v3+json"})
         if mod_file.status_code == 404:
-            logger.error("Writing to gist fail: " + json.loads(mod_file.text)['message'])
+            logger.error(f"Writing to gist fail: {json.loads(mod_file.text)['message']}. A new gist will be created.")
             create()
         elif mod_file.status_code == 200:
-            logger.info("Writing to gist success!\nRaw URL: " + generate_url(json.loads(mod_file.text)['files']
-                                                                             ['stairunlocker']['raw_url']))
+            logger.info("Writing to gist success! Please visit: https://gists.github.com for details.")
         else:
-            logger.error("Writing to gist fail: " + json.loads(mod_file.text)['message'])
+            logger.error(f"Writing to gist fail: {json.loads(mod_file.text)['message']}")
+            raise json.loads(mod_file.text)['message']
